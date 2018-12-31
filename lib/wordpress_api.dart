277682be845c5770ab4d17fb85a77134ce4055be
Douglas Harrington Muhone <theeomm@outlook.com>
2018-12-31 16:17:31 +0800

    remove _ from site, removed hasError & hasData

--- lib/wordpress_api.dart
+++ lib/wordpress_api.dart
@@ -4,24 +4,22 @@ import 'dart:convert';
 import 'package:http/http.dart' show Client;
 
 class WordPressAPI {
-  String _site, _link;
-  String apiNamespace = 'wp/v2';
-  bool hasError = false;
-  bool hasData = false;
+  String site, _link;
+  String _apiNamespace = 'wp/v2';
+  final bool isWooCommerce;
   final String consumerKey, consumerSecret;
   final Client _client = Client();
-  final bool isWooCommerce;
 
   // Initialization
   WordPressAPI(
-    this._site, {
+    this.site, {
     this.consumerKey = '',
     this.consumerSecret = '',
     this.isWooCommerce = false,
   }) {
-    if (!_site.startsWith('http')) {
-      _site = 'http://$_site'.toLowerCase();
-      print('SITE: $_site');
+    if (!site.startsWith('http')) {
+      site = 'http://$site'.toLowerCase();
+      // print('SITE: $site');
     }
   }
 
@@ -40,33 +38,33 @@ class WordPressAPI {
     if (namespace.isNotEmpty) {
       // CHECK IF NAMESPACE HAS A TRAILING SLASH
       if (namespace.endsWith('/')) {
-        apiNamespace =
+        _apiNamespace =
             namespace.substring(0, namespace.length - 1).toLowerCase();
-        _link = '$url$apiNamespace/$endpoint';
-        print('NAMESPACED LINK: $_link');
+        _link = '$url$_apiNamespace/$endpoint';
+        // print('NAMESPACED LINK: $_link');
       } else {
-        apiNamespace = namespace;
-        _link = '$url$apiNamespace/$endpoint';
-        print('NAMESPACED LINK: $_link');
+        _apiNamespace = namespace;
+        _link = '$url$_apiNamespace/$endpoint';
+        // print('NAMESPACED LINK: $_link');
       }
-    } else {
-      _link = '$url$apiNamespace/$endpoint';
-      print('GET LINK: $_link');
     }
 
     if (isWooCommerce) {
       String credentials =
           'consumer_key=$consumerKey&consumer_secret=$consumerSecret';
       // SET WOOCOMMERCE NAMESPACE
-      apiNamespace = 'wc/v3/';
+      _apiNamespace = 'wc/v3/';
       // CHECK IF ENDPOINT HAS A QUERY
       if (endpoint.contains('?')) {
-        _link = '$url$apiNamespace$endpoint&$credentials';
-        print('WC /?: $_link');
+        _link = '$url$_apiNamespace$endpoint&$credentials';
+        // print('WC /?: $_link');
       } else {
-        _link = '$url$apiNamespace$endpoint?$credentials';
-        print('WC URL: $_link');
+        _link = '$url$_apiNamespace$endpoint?$credentials';
+        // print('WC URL: $_link');
       }
+    } else {
+      _link = '$url$_apiNamespace/$endpoint';
+      // print('GET LINK: $_link');
     }
 
     try {
@@ -79,31 +77,20 @@ class WordPressAPI {
           'totalPages': int.parse(res.headers['x-wp-totalpages'])
         }
       };
-      // TOGGLE HAS ERROR
-      hasData = true;
-      print('HAS DATA SET TO TRUE');
       return data;
-    } on Exception catch (e) {
-      throw Exception(e);
     } catch (e) {
-      hasError = true;
-      print('HAS ERROR SET TO TRUE');
-      return {
-        'error': {'message': e}
-      };
+      throw Exception(e);
     }
   }
 
   // DISCOVER API LINK FROM HEADER
   Future<String> _getLink() async {
     try {
-      final res = await _client.head(_site);
+      final res = await _client.head(site);
       final links = res.headers['link'].split(';')[0];
       return links.substring(1, links.length - 1);
-    } on Exception catch (e) {
-      throw Exception(e);
     } catch (e) {
-      throw e;
+      throw Exception(e);
     }
   }
 }
