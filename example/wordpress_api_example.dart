48d40c5a73fb194bbaef5e74ae4d0415043a8d6f
Douglas Harrington Muhone <doug@dhmgroup.net>
2021-09-22 08:42:33 +0200

    updated example url

--- example/wordpress_api_example.dart
+++ example/wordpress_api_example.dart
@@ -1,7 +1,7 @@
 import 'package:wordpress_api/wordpress_api.dart';
 
 void main() async {
-  final api = WordPressAPI('mwebantu.com');
+  final api = WordPressAPI('site.com');
   final res = await api.posts.fetch();
 
   for (final post in res.data as List<Post>) {
